56bbcc1b0054f7d3acb55fcf598fde38202fca44
Adi Chaudhary <72666358+adichaudhary@users.noreply.github.com>
2025-01-11 11:56:52 -0500

    Delete Project 1.5 directory

--- Project 1.5/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module jgoodman.Template {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-	requires javafx.base;
-
-    opens template to javafx.fxml;
-    exports template;
-}
